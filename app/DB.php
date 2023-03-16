474608215a028f7917c656fa3b5c4070051e804b
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2023-03-16 22:19:46 +0630

    a

---
+++ app/DB.php
@@ -0,0 +1,25 @@
+<?php 
+
+class DB{
+    private $host = "localhost:3308";
+    private $dbname = "wlk-blog";
+    private $username = "root";
+    private $password = "";
+    private $connection;
+
+    public function connect(){
+        try{
+            $this->connection = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
+            echo "Connection Success";
+            return $this->connection;
+        }catch(Exception $e){
+            echo "Connection Fail";
+        }
+    }
+
+}
+$db = new DB();
+$db->connect();
+
+
+?>
---
+++ app/QueryBuilder.php
@@ -0,0 +1,95 @@
+<?php 
+
+class QueryBuilder{
+    private $conn;
+    public function __construct($connection){
+        $this->conn = $connection;
+        return $this->conn;
+    }
+
+    //create
+    public function create($table, $datas){
+        $column_names = implode("," , array_keys($datas));
+        $bind_values = implode(", :", array_keys($datas));
+        $sql = "INSERT INTO $table($column_names) VALUES(:$bind_values)";
+
+        $stmt = $this->conn->prepare($sql);
+        foreach($datas as $key => &$value){
+            $stmt->bindParam(":".$key, $value);
+        }
+        $stmt->execute();
+        return true;
+    }
+
+    //login
+    public function login($email, $password){
+        $state = $this->conn->prepare("SELECT * FROM users WHERE email=:email");
+        $state->bindParam(":email", $email);
+        $state->execute();
+        $result = $state->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+
+    //getAllJoin
+    public function getAll($table, $cols, $join, $where, $order){
+        $sql = "SELECT $cols FROM $table";
+        if($join != null){
+            $sql .= " $join";
+        }
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
+        if($order != null){
+            $sql .= " ORDER BY $order";
+        }
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $results;
+    
+    }
+    //getoneJoin
+    public function get($table, $cols, $join, $where){
+        $sql = "SELECT $cols FROM $table";
+        if($join != null){
+            $sql .= " $join";
+        }
+        if($where != null){
+            $sql .= " WHERE $where";
+        }
+        // echo $sql;
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
+    //update
+    public function update($table, $datas, $id){
+        $edits = "";
+        foreach($datas as $key=>$value){ 
+            $edits .= "$key=:$key,"; 
+        }
+        $edits = rtrim($edits, ',');
+        $sql = "UPDATE $table SET $edits WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        foreach($datas as $key => &$value){
+            $stmt->bindParam(":".$key, $value);
+        }
+        $stmt->execute();
+        return true;
+    }
+
+    //delete
+    public function delete($table, $id){
+        $sql = "DELETE FROM $table WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return true;
+    }
+}
+
+?>
